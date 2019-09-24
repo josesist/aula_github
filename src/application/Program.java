920afc54dff0fc49508133dd67f3e4e1c0e9d25e
José Carlos <josesist@gmail.com>
2019-09-24 20:59:37 -0300

    add boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import com.sun.tools.sjavac.server.SysInfo;
+
 import entities.Product;
 
 public class Program{
@@ -12,7 +14,7 @@ public class Program{
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("BOm DIA");
-		
+		System.out.println("boa tarde");
 		
 		
 		
